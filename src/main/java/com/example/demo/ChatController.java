c899dbe686087703ccf8e32dc2e532581f4255b5
shresth-sahai <shresth0407@gmail.com>
2025-06-14 11:14:35 +0530

    code added

---
+++ src/main/java/com/example/demo/ChatController.java
@@ -0,0 +1,25 @@
+package com.example.demo;
+
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDateTime;
+
+@Controller
+public class ChatController {
+    
+    @MessageMapping("/send")
+    @SendTo("/topic/messages")
+    public ChatMessage send(ChatMessage message){
+        message.setTimestamp(LocalDateTime.now().toString());
+        return message;
+    }
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    
+    
+}
